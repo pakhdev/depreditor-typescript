b686da65b2948d3e705da0aea035ec5728bd1a8b
pakhdev <t604977944@gmail.com>
2024-01-29 21:26:49 +0100

    Creación de métodos para obtener y comparar nodos seleccionados

--- src/modules/node-inspector.ts
+++ src/modules/node-inspector.ts
@@ -1,6 +1,8 @@
 import { EditorInitializer } from './editor-Initializer.ts';
 import { FormattingName } from '../types';
 
+type NodeOrFalse = Node | false | NodeOrFalse[];
+
 export class NodeInspector {
 
     constructor(private readonly depreditor: EditorInitializer) {
@@ -33,5 +35,72 @@ export class NodeInspector {
         const childElements = node.childNodes;
         if (childElements.length === 0) return true;
         return childElements.length === 1 && node.firstChild?.nodeName.toLowerCase() === 'br';
+    }
+
+    // Devuelve los nodos seleccionados completos, aunque no estén completamente seleccionados
+    public getSelectedNodes(): Node[] {
+        const selection = window.getSelection();
+        const nodes: Node[] = [];
+
+        if (selection) {
+            const range = selection.getRangeAt(0);
+            const commonAncestor = range.commonAncestorContainer;
+            const { startContainer, endContainer } = range;
+
+            let startContainerFound = false;
+            for (const node of commonAncestor.childNodes) {
+
+                if (!startContainerFound && this.isNodeInAncestor(startContainer, node))
+                    startContainerFound = true;
+
+                if (startContainerFound) nodes.push(node);
+
+                if (this.isNodeInAncestor(endContainer, node)) break;
+            }
+        }
+        return nodes;
+    }
+
+    // Devuelve los nodos seleccionados completos, pero solo con el contenido seleccionado
+    public getSelectedFragment(): Node[] {
+        const selection = window.getSelection();
+        if (!selection) return [];
+        const range = selection.getRangeAt(0);
+        const fragment = range.cloneContents();
+        return Array.from(fragment.childNodes);
+    }
+
+    public isNodeInAncestor(node: Node, ancestor: Node): boolean {
+        if (node === ancestor) return true;
+        while (node.parentNode && node.parentNode !== ancestor) {
+            node = node.parentNode;
+        }
+        return node.parentNode === ancestor;
+    }
+
+    public compareChildNodes(originalChildNodes: Node[], copyChildNodes: Node[]): NodeOrFalse[] {
+        const nodesMapping: NodeOrFalse[] = [];
+        for (let i = 0; i < originalChildNodes.length; i++) {
+
+            const originalChild = originalChildNodes[i];
+            const copyChild = copyChildNodes[i];
+
+            if (!copyChild) {
+                nodesMapping.push(originalChild);
+                continue;
+            }
+
+            if (originalChild.isEqualNode(copyChild)) {
+                nodesMapping.push(false);
+            } else if (originalChild.childNodes.length) {
+                const childNodesMapping = this.compareChildNodes(Array.from(originalChild.childNodes), Array.from(copyChild.childNodes));
+                childNodesMapping.length === originalChild.childNodes.length
+                    ? nodesMapping.push(originalChild)
+                    : nodesMapping.push(childNodesMapping);
+            } else {
+                nodesMapping.push(originalChild);
+            }
+        }
+        return nodesMapping;
     }
 }
