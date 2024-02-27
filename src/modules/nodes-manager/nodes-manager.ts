2077471711468d7bc20f92c8fe11576de2e02df8
pakhdev <t604977944@gmail.com>
2024-02-27 19:39:28 +0100

    Creada una estructura básica para el nuevo módulo NodesManager

---
+++ src/modules/nodes-manager/nodes-manager.ts
@@ -0,0 +1,56 @@
+export class NodesManager {
+
+    private nodesBackup;
+    private nodes;
+
+    // ** =========================
+    // *  Carga de nodos
+    // ** =========================
+
+    public setSelection(): void {}
+
+    public findAffectedNodes(): void {}
+
+    // ** =========================
+    // *  Creación de Backup
+    // ** =========================
+
+    public makeBackup(activate: boolean): void {
+        if (!activate) return;
+    }
+
+    // ** =========================
+    // *  Filtración de nodos
+    // ** =========================
+
+    public filterSelectedNodes(): void {}
+
+    public filterFormattedNodes(): void {}
+
+    public filterUnformattedNodes(): void {}
+
+    // ** =========================
+    // *  Modificación de nodos
+    // ** =========================
+
+    public detachSelectedFragment(): void {}
+
+    public applyFormat(): void {}
+
+    public removeFormat(): void {}
+
+    // ** =========================
+    // *  Retorno de datos
+    // ** =========================
+
+    public getBackup(): void {}
+
+    public getNodes(): void {}
+
+    public getPreviousNode(): void {}
+
+    public getNextNode(): void {}
+
+    public getFormattingList(): void {}
+
+}
