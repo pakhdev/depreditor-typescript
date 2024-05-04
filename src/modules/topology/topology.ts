ce0b8f77cf16502f9e05d0f6aad2a96771e025ea
pakhdev <t604977944@gmail.com>
2024-05-04 11:24:27 +0200

    - Desvinculada la clase Topology de NodeSelection (ya no la extiende) - Pasadas las propiedades a la clase Topology - Pasados y adaptados los getters a la clase Topology

--- src/modules/topology/topology.ts
+++ src/modules/topology/topology.ts
@@ -1,14 +1,61 @@
-import { getNodePath } from '../../helpers/nodeRouter.helper.ts';
-import { SelectionArgs } from './interfaces/selection-args.interface.ts';
-import { NodeSelection } from '../selection-manager/node-selection.ts';
-import { SelectionManager } from '../selection-manager/selection-manager.ts';
+import { getNodePosition } from '../../helpers/nodeRouter.helper.ts';
 
-export class Topology extends NodeSelection {
+export class Topology {
+
+    public parent: Topology | null = null;
+    public children: Topology[] = [];
+    public topologyToPreserve: Topology | null = null;
+    public textBeforeSelection: string = '';
+    public textWithinSelection: string = '';
+    public textAfterSelection: string = '';
 
     public path: number[] = [];
-    public children: Topology[] = [];
-    public parent: Topology | null = null;
-    public topologyToPreserve: Topology | null = null;
+
+    constructor(public node: Node) {}
+
+    public get start(): number {
+        if (this.node.nodeType === Node.TEXT_NODE)
+            return this.textBeforeSelection.length;
+        else if (this.children.length > 0)
+            return getNodePosition(this.children[0].node, this.node);
+        return 0;
+    }
+
+    public get end(): number {
+        if (this.node.nodeType === Node.TEXT_NODE)
+            return this.textBeforeSelection.length + this.textWithinSelection.length;
+        else if (this.children.length > 0)
+            return getNodePosition(this.children[0].node, this.node) + 1;
+        return 0;
+    }
+
+    public get length(): number {
+        return this.node.nodeType === Node.TEXT_NODE
+            ? this.node.textContent!.length
+            : this.node.childNodes.length;
+    }
+
+    public get isRange(): boolean {
+        return this.start !== this.end;
+    }
+
+    public get fullySelected(): boolean {
+        return this.startSelected && this.endSelected;
+    }
+
+    public get startSelected(): boolean {
+        return this.start === 0;
+    }
+
+    public get endSelected(): boolean {
+        return !this.length || this.end === this.length;
+    }
+
+    public get parentNode(): Node {
+        if (!this.node.parentNode)
+            throw new Error('El nodo no tiene un nodo padre');
+        return this.node.parentNode;
+    }
 
     /**
      * Comprueba si la topología ha sido insertada en el DOM.
@@ -28,46 +75,10 @@ export class Topology extends NodeSelection {
         else return this.children[this.children.length - 1].lastSelected;
     }
 
-    get textContent(): string {
+    public get textContent(): string {
         if (!this.node || this.node.nodeType !== Node.TEXT_NODE)
             return '';
         return this.node.textContent || '';
-    }
-
-    /**
-     * Crea una topología. Solo se asigna el nodo y el final de la selección.
-     * El resto de propiedades se tiene que asignar fuera de este método.
-     */
-    public fromNode(node: Node): Topology {
-        this.setNode(node).setEnd(this.length ? this.length : 0);
-        return this;
-    }
-
-    /**
-     * Crea una topología a partir de una selección.
-     * Adicionalmente, se crea y se asigna una topología del nodo padre del nodo común.
-     */
-    public fromSelection(selection: SelectionManager): Topology {
-        const ancestorPath = getNodePath(selection.commonAncestor, selection.editableDiv);
-        if (!ancestorPath)
-            throw new Error('No se encontró el ancestro común');
-
-        this.fromNode(selection.commonAncestor).setPath(ancestorPath);
-
-        const selectionArgs: SelectionArgs = { selection, startFound: { value: false } };
-        if (this.node.nodeType === Node.TEXT_NODE) this.scanTextNode(selectionArgs);
-        else this.scanElementNode(selectionArgs);
-
-        if (ancestorPath.length > 0 && this.node.parentNode) {
-            const parentTopology = new Topology()
-                .fromNode(this.node.parentNode)
-                .setPath(ancestorPath.slice(0, -1))
-                .setChildren([this])
-                .recalculateSelection();
-            this.setParent(parentTopology);
-        }
-
-        return this;
     }
 
     public setNode(node: Node): Topology {
@@ -95,59 +106,6 @@ export class Topology extends NodeSelection {
             ? this.findByNode(parentToPreserve)
             : parentToPreserve;
         return this;
-    }
-
-    public scanTextNode(selectionArgs?: SelectionArgs): void {
-        if (!selectionArgs) return;
-        const { selection } = selectionArgs;
-        if (selection.startNode.node === this.node || selection.endNode.node === this.node) {
-            const selectedNode = selection.startNode.node === this.node
-                ? selection.startNode
-                : selection.endNode;
-            this.setStart(selectedNode.start).setEnd(selectedNode.end);
-            if (selectedNode.parentToPreserve) this.setTopologyToPreserve(selectedNode.parentToPreserve);
-            selectionArgs.startFound.value = true;
-        }
-    }
-
-    public scanElementNode(selectionArgs?: SelectionArgs): void {
-        let startNode: Node | null = null;
-        let endNode: Node | null = null;
-        let startFound = { value: true };
-        if (selectionArgs) {
-            const { selection } = selectionArgs;
-            startFound = selectionArgs.startFound;
-            startNode = selection.startNode.node;
-            endNode = selection.endNode.node;
-        }
-        const children = Array.from(this.node!.childNodes);
-
-        for (let i = 0; i < children.length; i++) {
-            const node = children[i];
-            if (!startFound.value && node !== startNode && !node.contains(startNode)) continue;
-            if (!startFound.value) this.setStart(i);
-
-            const topology = new Topology()
-                .fromNode(node)
-                .setPath([...this.path, i])
-                .setParent(this);
-
-            node.nodeType === Node.TEXT_NODE
-                ? topology.scanTextNode(selectionArgs)
-                : topology.scanElementNode(selectionArgs);
-
-            if (node === startNode && node.nodeType !== Node.TEXT_NODE) {
-                startFound.value = true;
-                topology.start = i;
-            }
-
-            if (selectionArgs?.selection.isRange || topology.node !== startNode || node.nodeType === Node.TEXT_NODE)
-                this.children.push(topology);
-            if (node === endNode || node.contains(endNode)) {
-                this.setEnd(i);
-                break;
-            }
-        }
     }
 
     public findByNode(nodeToFind: Node, topology?: Topology): Topology | null {
@@ -187,42 +145,6 @@ export class Topology extends NodeSelection {
         this.parent.children.splice(topologyChildIdx, 1);
         this.parentNode.removeChild(this.node);
         this.parent.recalculateSelection().recalculatePaths();
-    }
-
-    /**
-     * Clona las propiedades de la topología y todas sus subtopologías.
-     * Se clonan los nodos y se sobreescribe la propiedad 'node'
-     * Si existiera una topología a preservar, se asignará una topología clonada de la misma.
-     */
-    public deepClone(topologyToPreserve: Topology | null = null, setParent: Topology | null = null): Topology {
-        const clonedNode = this.node.cloneNode();
-        const { nodeType } = clonedNode;
-
-        const clonedTopology = new Topology()
-            .fromNode(clonedNode);
-
-        if (nodeType === Node.TEXT_NODE && !this.fullySelected)
-            clonedTopology.setTopologyToPreserve(topologyToPreserve);
-
-        if (nodeType === Node.ELEMENT_NODE) {
-            for (const childTopology of this.children) {
-                if (!topologyToPreserve)
-                    topologyToPreserve = this;
-                if (!setParent)
-                    setParent = clonedTopology;
-                const clonedChild = childTopology.deepClone(topologyToPreserve, setParent);
-                clonedTopology.children.push(clonedChild);
-                clonedNode.appendChild(clonedChild.node);
-            }
-        }
-
-        clonedTopology
-            .setParent(this.parent)
-            .setPath(this.path)
-            .setStart(this.start)
-            .setEnd(this.end);
-
-        return clonedTopology;
     }
 
     // Recalcula las rutas de la topología y todas sus subtopologías.
