a0a8824f84d429c62bc259f877a7c3297759f4c6
pakhdev <t604977944@gmail.com>
2024-06-14 13:19:30 +0200

    - Nueva clase SelectedElement que sustituye la clase NodeSelection. Ahora solo guardará la ruta hacia el nodo y tendrá un getter para obtener el nodo. Será util para restaurar la selección por coordenadas si el nodo antiguo ha sido sustituido - Eliminada la clase NodeSelection

---
+++ src/core/selection/helpers/selected-element.ts
@@ -0,0 +1,45 @@
+export class SelectedElement {
+    public readonly path: number[];
+    public offset: { start: number, end: number }
+
+    constructor(
+        public readonly ownerEditor: HTMLDivElement,
+        node: Node,
+        offset: { start: number, end?: number }
+    ) {
+        this.path = this.calculatePath(node);
+        if (offset.end === undefined)
+            offset.end = node.nodeType === Node.TEXT_NODE
+                ? (node as Text).length
+                : 0;
+        this.offset = { start: offset.start, end: offset.end };
+    }
+
+    public get node(): Node {
+        let node: Node = this.ownerEditor;
+        for (const index of this.path) {
+            if (node.childNodes.length <= index)
+                throw new Error('El índice no existe en el nodo');
+            node = node.childNodes[index];
+        }
+        return node;
+    }
+
+    private calculatePath(node: Node): number[] {
+        const path: number[] = [];
+        let currentNode: Node | null = node;
+
+        while (currentNode !== this.ownerEditor && currentNode?.parentNode) {
+            const index = Array.from(currentNode.parentNode.childNodes).indexOf(currentNode as ChildNode);
+            if (index !== -1)
+                path.unshift(index);
+            currentNode = currentNode.parentNode;
+        }
+
+        if (currentNode !== this.ownerEditor)
+            throw new Error('El nodo no se encuentra en el editor');
+
+        return path;
+    }
+
+}
