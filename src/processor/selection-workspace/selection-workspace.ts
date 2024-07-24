3ec1118e9d0bf10cddf109de013cbe48754ced90
pakhdev <t604977944@gmail.com>
2024-07-24 11:44:17 +0200

    - Creada la clase WorkspaceExtender con métodos selectFully y coverNode para extender la selección de la clase SelectionWorkspace - Actualizada la clase SelectionWorkspace para el uso de la clase WorkSpaceExtender

--- src/processor/selection-workspace/selection-workspace.ts
+++ src/processor/selection-workspace/selection-workspace.ts
@@ -3,9 +3,10 @@ import FragmentsCloner from './helpers/fragments-cloner.ts';
 import FragmentsFinder from './helpers/fragments-finder/fragments-finder.ts';
 import SelectionStateType from '../../core/selection/enums/selection-state-type.enum.ts';
 import StoredSelection from '../../core/selection/helpers/stored-selection.ts';
+import WorkspaceExtender from './helpers/workspace-extender.ts';
 
 class SelectionWorkspace {
-    private selection: StoredSelection;
+    private readonly selection: StoredSelection;
 
     constructor(private readonly core: Core) {
         const { commonAncestor, startElement, endElement } = this.core.selection.get(SelectionStateType.CURRENT);
@@ -18,5 +19,9 @@ class SelectionWorkspace {
 
     public get cloneFragment(): FragmentsCloner {
         return new FragmentsCloner(this.core);
+    }
+
+    public get extend(): WorkspaceExtender {
+        return new WorkspaceExtender(this.selection);
     }
 }
