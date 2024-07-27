ad88fbcf52edda769abdb66c3d6eced12b931e52
pakhdev <t604977944@gmail.com>
2024-07-27 14:57:55 +0200

    Creados los métodos selectChar, selectNextChar y selectPreviousChar en la clase WorkspaceExtender

--- src/processor/selection-workspace/helpers/workspace-extender.ts
+++ src/processor/selection-workspace/helpers/workspace-extender.ts
@@ -1,7 +1,13 @@
 import StoredSelection from '../../../core/selection/helpers/stored-selection.ts';
+import ContainerProperties from '../../../core/containers/interfaces/container-properties.interface.ts';
+import Core from '../../../core/core.ts';
+import SelectedElement from '../../../core/selection/helpers/selected-element.ts';
 
 class WorkspaceExtender {
-    constructor(private readonly workspaceSelection: StoredSelection) {}
+    constructor(
+        private readonly workspaceSelection: StoredSelection,
+        private readonly core: Core,
+    ) {}
 
     public selectFully(): void {
         const { startElement, endElement } = this.workspaceSelection;
@@ -22,6 +28,54 @@ class WorkspaceExtender {
             throw new Error('No se puede cubrir el nodo porque no tiene nodo padre');
         this.workspaceSelection.setCommonAncestorNode(node.parentNode);
     }
+
+    public selectNext(restrictedContainers: ContainerProperties[]): void {
+        if (!this.workspaceSelection.isNothingSelected)
+            return;
+
+        if (this.workspaceSelection.commonAncestor.node.nodeType === Node.TEXT_NODE) {
+            if (this.selectNextChar(this.workspaceSelection.startElement))
+                return;
+        }
+        // Select next node
+    }
+
+    private selectPrevious(restrictedContainers: ContainerProperties[]): void {
+        if (!this.workspaceSelection.isNothingSelected)
+            return;
+
+        if (this.workspaceSelection.commonAncestor.node.nodeType === Node.TEXT_NODE) {
+            if (this.selectPreviousChar(this.workspaceSelection.startElement))
+                return;
+        }
+        // Select previous node
+    }
+
+    private selectChar(selectedElement: SelectedElement, isNext: boolean): boolean {
+        if (selectedElement.node.nodeType === Node.TEXT_NODE) {
+            const textNode = selectedElement.node as Text;
+            const { start, end } = selectedElement.offset;
+            const maxLength = textNode.length;
+            const newOffset = isNext ? end + 1 : start - 1;
+
+            if ((isNext && newOffset <= maxLength) || (!isNext && newOffset >= 0)) {
+                this.workspaceSelection.updateAllSelectionPoints({
+                    offset: isNext ? { end: newOffset } : { start: newOffset },
+                });
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private selectNextChar(selectedElement: SelectedElement): boolean {
+        return this.selectChar(selectedElement, true);
+    }
+
+    private selectPreviousChar(selectedElement: SelectedElement): boolean {
+        return this.selectChar(selectedElement, false);
+    }
+
 }
 
 export default WorkspaceExtender;
