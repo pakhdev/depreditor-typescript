d08247faa4e9881d53750d5c112e7f433a3d7a45
pakhdev <t604977944@gmail.com>
2024-10-09 10:37:52 +0200

    Terminado el handler del evento internalDrop

--- src/interaction/editor-events/handlers/internal-drop.ts
+++ src/interaction/editor-events/handlers/internal-drop.ts
@@ -1,20 +1,28 @@
 import Processor from '../../../processor/processor.ts';
 import HookHandler from '../../../core/event-hooks/interfaces/hook-handler.interface.ts';
-import TextPreprocessor from '../helpers/text-preprocessor.ts';
+import SelectionWorkspace from '../../../processor/selection-workspace/selection-workspace.ts';
+import SelectionStateType from '../../../core/selection/enums/selection-state-type.enum.ts';
+import OperationsBuilder from '../../../processor/utilities/operations-builder/operations-builder.ts';
+import AffectedNodesPart from '../../../core/selection/enums/affected-nodes-part.enum.ts';
 
 const internalDrop: HookHandler = (event?: Event, processor?: Processor): void => {
-    if (!event || !processor)
-        throw new Error('Internal: El evento o el procesador no están definidos');
+    if (!event || !processor) throw new Error('Internal drag: El evento o el procesador no están definidos');
+
     const e = event as DragEvent;
     e.preventDefault();
-    e.dataTransfer!.getData('text');
-    const dataTransfer = e.dataTransfer;
-    if (!dataTransfer) return;
-    let text = new TextPreprocessor(dataTransfer.getData('text')).sanitize();
-    if (text.containsBreakLine())
-        processor.commandHandler.insertNodes(text.getAsHtml());
-    else
-        processor.commandHandler.handleText(text.getAsText());
+
+    const previousSelectionWorkspace = new SelectionWorkspace(processor.core, SelectionStateType.PREVIOUS);
+    const operationsBuilder = new OperationsBuilder(previousSelectionWorkspace);
+
+    const injectingOperations = previousSelectionWorkspace.selection.commonAncestor.isTextNode ? [] :
+        operationsBuilder.injectNodes([
+            ...previousSelectionWorkspace.cloneFragment.selectedPart(AffectedNodesPart.BEFORE).nodes,
+            ...previousSelectionWorkspace.cloneFragment.selectedPart(AffectedNodesPart.AFTER).nodes,
+        ]);
+    const removalOperations = operationsBuilder.removeSelected();
+    const selectedPart = previousSelectionWorkspace.cloneFragment.selectedPart(AffectedNodesPart.WITHIN).nodes;
+
+    processor.commandHandler.handleInsertion(selectedPart, injectingOperations, removalOperations);
 };
 
 export default internalDrop;
